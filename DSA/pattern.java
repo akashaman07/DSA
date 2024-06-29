da3d5d26154c0beea17cfeffad1762c0014e6a38
Akash Aman <147074643+akashaman07@users.noreply.github.com>
2024-06-29 10:24:15 +0530

    Add files via upload

--- DSA/pattern.java
+++ DSA/pattern.java
@@ -74,10 +74,28 @@ public class pattern {
         }
     }
 
+    static void p8(int n) {
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2*n-(2 * i + 1); j++) {
+
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n = sc.nextInt();
-        p7(n);
+        p8(n);
     }
 }
