fa19205745fbabff80e28136bdaabb0f99bc5ea2
Akash Aman <147074643+akashaman07@users.noreply.github.com>
2024-07-01 23:17:07 +0530

    Add files via upload

--- DSA/Array/Largest_Element.java
+++ DSA/Array/Largest_Element.java
@@ -1,24 +1,25 @@
 package Array;
+
 import java.util.*;
 
 public class Largest_Element {
- 
+
   public static void main(String args[]) {
- 
-    int arr1[] =  {2,5,1,3,0};
-    System.out.println("The Largest element in the array is: "+findLargestElement(arr1));
- 
-    int arr2[] =  {8,10,5,7,9};
-    System.out.println("The Largest element in the array is: "+findLargestElement(arr2));
+
+    int arr1[] = { 2, 5, 1, 3, 0 };
+    System.out.println("The Largest element in the array is: " + findLargestElement(arr1));
+
+    int arr2[] = { 8, 10, 5, 7, 9 };
+    System.out.println("The Largest element in the array is: " + findLargestElement(arr2));
   }
+
   static int findLargestElement(int arr[]) {
-    int max= arr[0];
+    int max = arr[0];
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] > max) {
-        max= arr[i];
+        max = arr[i];
       }
     }
     return max;
   }
 }
-
