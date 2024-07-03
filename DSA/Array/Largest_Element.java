64b2954cb9aa5b0e744f03719109d435ec9a6c88
Akash Aman <147074643+akashaman07@users.noreply.github.com>
2024-07-03 14:49:52 +0530

    Add files via upload

--- DSA/Array/Largest_Element.java
+++ DSA/Array/Largest_Element.java
@@ -1,6 +1,22 @@
-package Array;
+/*Problem Statement: Given an array, we have to find the largest element in the array.
+Example 1:
+Input:
+ arr[] = {2,5,1,3,0};
+Output:
+ 5
+Explanation:
+ 5 is the largest element in the array. 
 
-import java.util.*;
+Example2:
+Input:
+ arr[] = {8,10,5,7,9};
+Output:
+ 10
+Explanation:
+ 10 is the largest element in the array. */
+
+
+package Array;
 
 public class Largest_Element {
 
